30fbe17d2227a8b846fccf17c23971ceac707575
livinglist <georgefung78@live.com>
2020-04-08 23:46:00 -0700

    Initial Commit

---
+++ CoronaCounter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoronaCounter
+//
+//  Created by Jiaqi Feng on 4/8/20.
+//  Copyright © 2020 Jiaqi Feng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CoronaCounter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoronaCounter
+//
+//  Created by Jiaqi Feng on 4/8/20.
+//  Copyright © 2020 Jiaqi Feng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
