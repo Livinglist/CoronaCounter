61652bfbd59fef8918ec5952b77b79c34dfac958
livinglist <georgefung78@live.com>
2020-04-11 22:24:19 -0700

    Fetch data from wikipedia instead of google's corona site since the later one is javascript rendered.

--- CoronaCounter/ViewController.swift
+++ CoronaCounter/ViewController.swift
@@ -14,7 +14,7 @@ import Alamofire
 struct Info{
     let confirms, recovers, deaths :String;
     
-    init(confrims confirms:String,recovers recovers:String,deaths deaths: String){
+    init(confirms confirms:String,recovers recovers:String,deaths deaths: String){
         self.confirms = confirms;
         self.recovers = recovers;
         self.deaths = deaths;
@@ -35,7 +35,7 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate{
         web = WKWebView(frame: .zero, configuration: webConfiguration)
         web.uiDelegate = self
         web.navigationDelegate = self
-        let myURL = URL(string:"https://google.com/covid19-map/?hl=en")
+        let myURL = URL(string:"https://en.wikipedia.org/wiki/Template:2019%E2%80%9320_coronavirus_pandemic_data#covid19-container")
         let myRequest = URLRequest(url: myURL!)
         web.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         web.load(myRequest)
@@ -62,42 +62,74 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate{
                                completionHandler: { (html: Any?, error: Error?) in
                                 if let doc = try? HTML(html: html as! String, encoding: .utf8) {
                                     print("below is the thing")
+
+
                                     
                                     self.statusItem.menu?.removeAllItems();
                                     
-                                    let countryNameSpans = doc.xpath("//*/c-wiz/div/div/div/div/div/div/div/div/div/div/div/table/tbody/tr/td/span")
+                                    //                                    let countryNameSpans = doc.xpath("//*/c-wiz/div/div/div/div/div/div/div/div/div/div/div/table/tbody/tr/td/span")
+                                    let countryNameSpans:XPathObject = doc.xpath("//*/tbody/tr/th[2]/a")
                                     
-                                    if countryNameSpans.count < 20{
-                                        self.loadItems()
-                                    }
+                    
+                                    //var countryNameSpans:[XPathObject] = []
+
+                                    print(countryNameSpans.count)
+                                    //print(countryNameSpans[225])
+
                                     
-                                    let confirmSpans = doc.xpath("//*/c-wiz/div/div/div/div/div/div/div/div/div/div/div/table/tbody/tr/td[2]")
-                                    let recoverSpans = doc.xpath("//*/c-wiz/div/div/div/div/div/div/div/div/div/div/div/table/tbody/tr/td[4]")
-                                    let deathSpans = doc.xpath("//*/c-wiz/div/div/div/div/div/div/div/div/div/div/div/table/tbody/tr/td[5]")
-                                    
-                                    let countryName = self.curCountry ?? countryNameSpans.first?.text;
-                                    self.curCountry = countryName;
-                                    
-                                    for i in 0..<countryNameSpans.count {
-                                        self.infoByCountryDict[countryNameSpans[i].text!] = Info(confrims: confirmSpans[i].text!, recovers: recoverSpans[i].text!, deaths: deathSpans[i].text!);
-                                        let item = NSMenuItem.init(title: countryNameSpans[i].text!, action:  #selector(self.printQuote(_:)), keyEquivalent: "");
-                                        item.target = self;
-                                        self.statusItem.menu?.addItem(item);
+                                    if countryNameSpans.count != 0{
                                         
-                                        print(countryNameSpans[i].text!)
-                                    }
-                                    
-                                    let confirmed = self.infoByCountryDict[countryName!]?.confirms;
-                                    let recovered = self.infoByCountryDict[countryName!]?.recovers;
-                                    let deaths = self.infoByCountryDict[countryName!]?.deaths;
-                                    
-                                    let item = NSMenuItem(title: "Quit CoronaCounter", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
-                                    
-                                    self.statusBarMenu.addItem(NSMenuItem.separator())
-                                    self.statusBarMenu.addItem(item)
-                                    
-                                    if let button = self.statusItem.button {
-                                        button.title = "\(countryName!): \(confirmed!) confirmed \(recovered!) recovered \(deaths!) deaths"
+                                        if countryNameSpans.count < 10{
+                                            self.loadItems()
+                                        }
+
+//                                        let worldConfirm = doc.css("#thetable > thead > tr:nth-child(2) > th:nth-child(3)")
+//                                        print(worldConfirm.first?.text!)
+//                                        let worldDeaths = doc.xpath("//*/thead/tr[2]/th[3]/span/b")[0].text
+//                                        let worldRecover = doc.xpath("//*/thead/tr[2]/th[4]/span/b")[0].text
+//
+                                        
+//                                        self.infoByCountryDict["Worldwide"] = Info(confirms: "", recovers: worldRecover!, deaths: worldDeaths!);
+//                                        let worldItem = NSMenuItem.init(title:"Worldwide", action:  #selector(self.printQuote(_:)), keyEquivalent: "");
+//                                        worldItem.target = self;
+//                                        self.statusItem.menu?.addItem(worldItem);
+                                        
+                                        let confirmSpans = doc.xpath("//*/tbody/tr/td[1]")
+                                        let recoverSpans = doc.xpath("//*/tbody/tr/td[3]")
+                                        let deathSpans = doc.xpath("//*/tbody/tr/td[2]")
+                                        
+                                        let countryName = self.curCountry ?? countryNameSpans.first?.text;
+                                        self.curCountry = countryName;
+                                        
+                                        for i in 0..<countryNameSpans.count {
+                                            self.infoByCountryDict[countryNameSpans[i].text!] = Info(confirms: confirmSpans[i].text!, recovers: recoverSpans[i].text!, deaths: deathSpans[i].text!);
+                                            let item = NSMenuItem.init(title: countryNameSpans[i].text!, action:  #selector(self.printQuote(_:)), keyEquivalent: "");
+                                            item.target = self;
+                                            self.statusItem.menu?.addItem(item);
+                                            
+                                            print(countryNameSpans[i].text)
+                                        }
+                                        
+                                        let confirmed = self.infoByCountryDict[countryName!]?.confirms;
+                                        let recovered = self.infoByCountryDict[countryName!]?.recovers;
+                                        let deaths = self.infoByCountryDict[countryName!]?.deaths;
+                                        
+                                        let quitItem = NSMenuItem(title: "Quit CoronaCounter", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+                                        
+                                        self.statusBarMenu.addItem(NSMenuItem.separator())
+                                        self.statusBarMenu.addItem(quitItem)
+                                        
+                                        if let button = self.statusItem.button {
+                                            print("\(countryName!): \(confirmed!) confirmed \(recovered!) recovered \(deaths!) deaths".replacingOccurrences(of: "\n", with: ""))
+                                            button.title = "\(countryName!): \(confirmed!) confirmed \(recovered!) recovered \(deaths!) deaths".replacingOccurrences(of: "\n", with: "")
+                                        }
+                                    }else{
+                                        if let button = self.statusItem.button {
+                                            button.title = "Failed to fetch data, please update software."
+                                        }
+                                        
+                                        let item = NSMenuItem(title: "Quit CoronaCounter", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+                                        self.statusBarMenu.addItem(item)
                                     }
                                 }
         })
@@ -134,7 +166,7 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate{
         let deaths = infoByCountryDict[item.title]?.deaths;
         let recovered = infoByCountryDict[item.title]?.recovers;
         
-        statusItem.button?.title = "\(item.title): \(confirmed!) confirmed \(recovered!) recovered \(deaths!) deaths";
+        statusItem.button?.title = "\(item.title): \(confirmed!) confirmed \(recovered!) recovered \(deaths!) deaths".replacingOccurrences(of: "\n", with: "");
     }
 }
 
